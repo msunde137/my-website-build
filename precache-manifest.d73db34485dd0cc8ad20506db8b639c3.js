de3bb86f9bc2cfcce58d0d99d903ae37331b5a12
Macintyre Sunde <imacsunde@gmail.com>
2023-03-08 22:19:12 -0500

    updated bio

---
+++ precache-manifest.d73db34485dd0cc8ad20506db8b639c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d42c5d5d635c92beffd9",
+    "url": "/static/css/main.1cd1067c.chunk.css"
+  },
+  {
+    "revision": "d42c5d5d635c92beffd9",
+    "url": "/static/js/main.d42c5d5d.chunk.js"
+  },
+  {
+    "revision": "a1fee0e1cd10b8944767",
+    "url": "/static/js/1.a1fee0e1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a2a0525eb69034eede817933b40b4367",
+    "url": "/index.html"
+  }
+];
